e9765f7e6b6282d7d11bc14fdefbc24ac6a39829
Robert Jackson <robertj@promedicalinc.com>
2012-06-14 10:01:44 -0400

    Make sure to require at least one indexed field.

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,5 +1,4 @@
 class ProvidersController < ApplicationController
-
   def search;end
 
   def results
@@ -11,9 +10,17 @@ class ProvidersController < ApplicationController
   end
 
   def index
-    @providers = Provider.first_name(params[:first_name]).
-                          last_name(params[:last_name]).
-                          npi(params[:npi]).
-                          page(params[:page])
+    if valid_search_fields
+      @providers = Provider.first_name(params[:first_name]).
+                            last_name(params[:last_name]).
+                            npi(params[:npi]).
+                            page(params[:page])
+    else
+      flash[:warning] = 'You must search using at least one of the following indexed fields (NPI, Org Name, Phone, Last Name).'
+    end
+  end
+
+  def valid_search_fields
+    params.any? { |key, value| [:npi, :organization, :phone, :last_name].includes?(key.to_sym) }
   end
 end
