ee2016d86ef4324722e1f00013c1e4472f58bb89
Robert Jackson <robertj@promedicalinc.com>
2012-06-15 17:41:28 -0400

    Show more details on provider taxonomies.

--- app/models/provider_taxonomy.rb
+++ app/models/provider_taxonomy.rb
@@ -1,7 +1,15 @@
 class ProviderTaxonomy < ActiveRecord::Base
   belongs_to :provider
 
+  def taxonomy_details
+    @taxonomy_details ||= Hippo::CodeLists::TaxonomyCodes[self.taxonomy]
+  end
+
   def description
-    Hippo::CodeLists::TaxonomyCodes[self.taxonomy]
+    taxonomy_details[:classification] + (taxonomy_details[:specialization].present? ? "/#{taxonomy_details[:specialization]}" : '')
+  end
+
+  def definition
+    taxonomy_details[:definition]
   end
 end
