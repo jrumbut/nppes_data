c0e42521629d850a48db9fe647341520f061227c
Robert Jackson <robertj@promedicalinc.com>
2012-06-17 11:04:30 -0400

    Add provider_identifiers to ProviderController show page.

--- app/models/provider_identifier.rb
+++ app/models/provider_identifier.rb
@@ -1,3 +1,16 @@
 class ProviderIdentifier < ActiveRecord::Base
   belongs_to :provider
+
+  def type
+    case type_code
+    when 'LC' then 'License'
+    when '01' then 'Other'
+    when '02' then 'Medicare Upin'
+    when '04' then 'Medicare ID - Other'
+    when '05' then 'Medicaid'
+    when '06' then 'Medicare Oscar/Cert'
+    when '07' then 'Medicare NSC'
+    when '08' then 'Medicare PIN'
+    end
+  end
 end
